d1fe4985c58025cece52a88e701ceb23c4a7a573
jpwsutton <jpwsutton@gmail.com>
2013-12-16 22:43:58 +0000

    Added matrix.py
    Basic file that will contain the driver

---
+++ matrix/matrix.py
@@ -0,0 +1,12 @@
+# Raspberry-pi-matrix driver
+
+
+def initMatrix():
+    print "Initializing Matrix Driver"
+	
+def writeArray():
+    print "Writing Array to Matrix"
+
+
+def clearMatrix():
+	print "Clearing Matrix"
